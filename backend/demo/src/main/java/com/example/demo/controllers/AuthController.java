e8f337622a3a698bea4459554730bd2e7780b0c7
AleksandrdaMqueen <99744766+AleksandrdaMqueen@users.noreply.github.com>
2025-07-17 00:47:54 +0300

    Update AuthController.java

--- backend/demo/src/main/java/com/example/demo/controllers/AuthController.java
+++ backend/demo/src/main/java/com/example/demo/controllers/AuthController.java
@@ -21,6 +21,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RestController
 public class AuthController {
